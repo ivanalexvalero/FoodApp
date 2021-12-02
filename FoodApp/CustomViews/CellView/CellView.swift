24406b1b9e6809030d29b1e039ea75eee1ac45a9
ivan <ivanvalero1985@gmail.com>
2021-12-01 21:48:25 -0300

    Nuevos avances en Home

---
+++ FoodApp/CustomViews/CellView/CellView.swift
@@ -0,0 +1,35 @@
+//
+//  CellView.swift
+//  FoodApp
+//
+//  Created by Ivan Valero on 01/12/2021.
+//
+
+import Foundation
+import UIKit
+import Accelerate
+
+
+class CellView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialSetup()
+    }
+    
+    private func initialSetup() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 0.1
+        layer.shadowRadius = 10
+        cornerRadius = 10
+    }
+    
+}
